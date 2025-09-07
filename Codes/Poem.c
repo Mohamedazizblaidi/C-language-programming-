1a89f7104885385b49df0f971b369962cc733611
Mohamed aziz blaidi <mohamedaziz5051@outlook.com>
2025-09-07 03:17:10 +0200

    Poem.c

---
+++ Codes/Poem.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("From microscope to machine, we see, \n");
+    printf("The wonders of biology and technology \n");
+    printf("Saving lives with innovation’s key. \n");
+
+    return 0;
+}
