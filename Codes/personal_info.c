2e3c342f292e6d4f1051ed04f4c670be672700ba
Mohamed aziz blaidi <mohamedaziz5051@outlook.com>
2025-09-07 15:59:40 +0200

    Personal_info.c

---
+++ Codes/personal_info.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // Declaration
+    int age;
+    float height;
+    char grade;
+    
+    // Initialization
+    age = 20;
+
+    // Declaration + Initialization
+    char Name[] = "Mohamed aziz";
+    char StudyField[] = "Biomedical Engineering";
+    
+    printf("Name: %s\n", Name);
+    printf("Age: %d\n", age);
+    printf("Field of Study: %s\n", StudyField);
+
+    
+    return 0;
+}
