cd82e69ce7270904cf89b85c56dbf1d4d162dbfc
Mohamed aziz blaidi <mohamedaziz5051@outlook.com>
2025-10-05 19:55:32 +0200

    Create Employee.c

---
+++ Class Exercises /Employee.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int age = 30;
+    double salaire = 2500.75;
+    
+    printf("Age: %d ans\n", age);        // %d pour entier
+    printf("Salaire: %.2f euros\n", salaire); // %f pour décimal
+    
+    return 0;
+}
