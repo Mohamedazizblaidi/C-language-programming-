751a6b8c0954742ffe619778006e7f1deee78f10
Mohamed aziz blaidi <mohamedaziz5051@outlook.com>
2025-10-05 19:59:22 +0200

    Update grade_evaluation.c

--- Class Exercises /grade_evaluation.c
+++ Class Exercises /grade_evaluation.c
@@ -5,3 +5,23 @@ if (grade >= 16) {
 } else {
     printf("Fail\n");
 }
+
+
+#include <stdio.h>
+
+int main() {
+    int grade;
+    
+    printf("Enter your grade: ");
+    scanf("%d", &grade);
+    
+    if (grade >= 16) {
+        printf("Excellent\n");
+    } else if (grade >= 10) {
+        printf("Good\n");
+    } else {
+        printf("Fail\n");
+    }
+    
+    return 0;
+}
